ef333bda694ecc94a788dc439c2d118b9990ab28
MauroBorzi <mauroborzi91@gmail.com>
2025-09-19 15:46:23 +0200

    [upd]HomePage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -5,8 +5,22 @@ const HomePage = () => {
     <div className="container">
       <div className="row">
         <div className="col-12">
-          <h1>BOOLBUSTER</h1>
-          <h5>La libreria di film più grande al mondo!</h5>
+          <div className='text-center py-4'>
+            <h1>BOOLBUSTER</h1>
+            <h5>La libreria di film più grande al mondo!</h5>
+          </div>
+        </div>
+      </div>
+      <div className="row gy-3">
+        <div className="col-12 col-md-6 col-lg-4 pt-4">
+          <div className="card-movie">
+            <img className='cover-movie' src="./lotr.png" alt="" />
+            <div className="overlay">
+              <h2>titolo anno</h2>
+              <h4>diretto</h4>
+              <p>descrizione</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
